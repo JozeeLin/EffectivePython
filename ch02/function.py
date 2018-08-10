d2084c3f6e213ef19fa36482a82c478b92354fd6
jozee <buxiaosheng123@gmail.com>
2018-08-11 01:04:07 +0800

    update ch02

--- ch02/function.py
+++ ch02/function.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # coding=utf-8
-#def divide(a,b):
-#    try:
-#        return a/b
-#    except ZeroDivisionError as e:
-#        raise ValueError('Invalid inputs') from e
-#
-#divide(1,0)
+import json
+
+
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError as e:
+        raise ValueError('Invalid inputs') from e
+
 
 def normalize(numbers):
     total = sum(numbers)
@@ -16,6 +18,44 @@ def normalize(numbers):
         result.append(percent)
     return result
 
-visits = [15,35,80]
-percentages = normalize(visits)
-print(percentages)
+
+def decode(data, default={'a':3}):
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+#通过使用动态参数的形式来解决这个问题
+def decode(data, default=None):
+    if default is None:
+        default = {}
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+
+if __name__ == '__main__':
+#    #第一个函数测试
+#    divide(1,0)
+#    #第二个函数测试
+#    visits = [15,35,80]
+#    percentages = normalize(visits)
+#    print(percentages)
+#
+#    #第三个函数测试
+#    foo = decode('bad data')
+#    foo['stuff'] = 5
+#    bar = decode('also bad')
+#    bar['meep'] = 1
+#    print('FOO:', foo)
+#    print('BAR:', bar)
+
+	#第四个函数测试
+    foo = decode('bad data')
+    foo['stuff'] = 5
+    bar = decode('also bad')
+    bar['meep'] = 1
+    print('Foo:', foo)
+    print('Bar:', bar)
+
